bc5f463086825398b371eed1c5a4b082b4b88330
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-12-22 16:59:29 +0200

    tweak opacity

--- src/lib/components/EndMenu.svelte
+++ src/lib/components/EndMenu.svelte
@@ -45,7 +45,7 @@
 		left: 0;
 		width: 100%;
 		height: 100%;
-		background-color: rgba(255, 255, 255, 0.75);
+		background-color: rgba(255, 255, 255, 0.8);
 		backdrop-filter: blur(5px);
 		z-index: 1;
 	}
