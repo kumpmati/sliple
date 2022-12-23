8351d6e43a16d989e28154b73dd2e351a02a1e70
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-12-23 02:58:13 +0200

    faster spring

--- src/lib/components/grid/tiles/TileWrapper.svelte
+++ src/lib/components/grid/tiles/TileWrapper.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import { GRID_CELL_SIZE } from '$lib/constants/grid';
 	import type { Tile } from '$lib/types/grid';
-	import { expoOut } from 'svelte/easing';
-	import { spring, tweened } from 'svelte/motion';
+	import { spring } from 'svelte/motion';
 
 	export let tile: Tile;
 	export let movable = false;
 	export let zIndex = 0;
 
-	const x = spring(tile.x, { stiffness: 0.15, damping: 0.6 });
-	const y = spring(tile.y, { stiffness: 0.15, damping: 0.6 });
+	const x = spring(tile.x, { stiffness: 0.25, damping: 0.6 });
+	const y = spring(tile.y, { stiffness: 0.25, damping: 0.6 });
 
 	// make sure values change when tile is updated
 	$: $x = tile.x;
@@ -17,7 +16,6 @@
 </script>
 
 <g
-	class="tile"
 	transform="translate({$x * GRID_CELL_SIZE}, {$y * GRID_CELL_SIZE})"
 	width={GRID_CELL_SIZE}
 	height={GRID_CELL_SIZE}
@@ -29,8 +27,8 @@
 </g>
 
 <style lang="scss">
-	.tile {
-		transition: all 100ms;
+	g {
+		will-change: transform;
 		pointer-events: none;
 
 		&.movable {
