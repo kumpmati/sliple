6198bba9590a42dfa0577b91f881f96941ac0cc3
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2023-01-02 13:17:33 +0200

    add text field component

---
+++ src/lib/components/TextField.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let value: string;
+	export let placeholder = '';
+	export let style = '';
+</script>
+
+<input type="text" bind:value {placeholder} {style} />
+
+<style lang="scss">
+	input {
+		border: 2px solid transparent;
+		border-radius: var(--border-radius-small);
+		background-color: var(--gray-light);
+		font-family: var(--font-heading);
+		color: var(--black);
+		font-size: 16px;
+		padding: 4px 8px;
+		outline: none;
+
+		&:focus {
+			border-color: var(--black);
+		}
+	}
+</style>
