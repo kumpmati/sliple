bace9eb42d601f586ca33591bfaeb8354b727ac9
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2023-01-10 22:44:23 +0200

    add campaign support to editor

--- src/routes/editor/+server.ts
+++ src/routes/editor/+server.ts
@@ -5,10 +5,11 @@ import z from 'zod';
 import { PuzzleModel } from '$lib/schemas/puzzle';
 import { nanoid } from 'nanoid';
 import { EDITOR_PASSWORD } from '$env/static/private';
-import { connectDB } from '$lib/services/database';
+import { connectDB, getCampaignById, updateCampaign } from '$lib/services/database';
 
 const bodySchema = z.object({
 	password: z.string(),
+	campaignId: z.string().nullable(),
 	level: gridSchema
 });
 
@@ -26,7 +27,25 @@ export const POST: RequestHandler = async ({ request }) => {
 		throw error(403, 'unauthorized');
 	}
 
+	// generate id for the level
 	const id = nanoid(15);
+
+	// if campaign is specified, add level to campaign instead.
+	const campaignId = body.data.campaignId;
+	if (campaignId) {
+		const campaign = await getCampaignById(campaignId);
+		if (!campaign) throw error(404, 'campaign not found');
+
+		campaign.levels.push({
+			id: id,
+			data: body.data.level,
+			message: null,
+			difficulty: campaign.difficulty
+		});
+
+		await updateCampaign(campaignId, campaign);
+		return json({ id, campaignId });
+	}
 
 	await new PuzzleModel({
 		id: id,
