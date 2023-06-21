d90f55e653853d4a23827e5d3b943b07c0c68be3
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2023-06-21 20:59:04 +0300

    remove algorithm change logic

--- src/routes/play/random/[seed]/+page.server.ts
+++ src/routes/play/random/[seed]/+page.server.ts
@@ -1,49 +1,17 @@
 import words from '$lib/assets/words.json';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import type { GeneratorConstraints } from '$lib/services/generator/interface';
-import type { Puzzle } from '$lib/types/puzzle';
 import { shufflingGenerator } from '$lib/services/generator/strategies/shuffling';
-import { reversibleGenerator } from '$lib/services/generator/strategies/reversible';
-import { randomGenerator } from '$lib/services/generator/strategies/legacy';
 
-export const load: PageServerLoad = async ({ params, url }) => {
-	const version = url.searchParams.get('v');
-
-	const constraints: GeneratorConstraints = {
-		words,
-		maxLength: 7,
-		minLength: 4
-	};
-
-	let puzzle: Puzzle;
-
-	const start = performance.now();
-
+export const load: PageServerLoad = async ({ params }) => {
 	try {
-		switch (version) {
-			case '3':
-			default: {
-				puzzle = shufflingGenerator.generate(params.seed, constraints);
-				break;
-			}
-
-			case '2': {
-				puzzle = reversibleGenerator.generate(params.seed, constraints);
-				break;
-			}
-
-			case '1': {
-				puzzle = randomGenerator.generate(params.seed, constraints);
-				break;
-			}
-		}
-
-		const end = performance.now();
-
-		console.log(end - start, 'ms');
-
-		return { puzzle };
+		return {
+			puzzle: shufflingGenerator.generate(params.seed, {
+				words,
+				maxLength: 7,
+				minLength: 4
+			})
+		};
 	} catch (err: any) {
 		throw error(500, err?.message ?? 'unknown error');
 	}
