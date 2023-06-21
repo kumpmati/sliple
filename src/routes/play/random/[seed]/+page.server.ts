79cedae5b9fff28cdcd2843f1138858f96f9cfcf
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2023-06-21 10:44:43 +0300

    add option to use old algorithm

--- src/routes/play/random/[seed]/+page.server.ts
+++ src/routes/play/random/[seed]/+page.server.ts
@@ -2,11 +2,31 @@ import words from '$lib/assets/words.json';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { generator } from '$lib/services/generator/v2';
+import { generatePuzzle } from '$lib/services/generator';
+import type { GeneratorConstraints } from '$lib/services/generator/interface';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, url }) => {
+	const version = url.searchParams.get('v') ?? '2';
+
+	const constraints: GeneratorConstraints = {
+		words,
+		maxLength: 7,
+		minLength: 4
+	};
+
 	try {
-		const puzzle = generator.generate(params.seed, { words, maxLength: 7, minLength: 4 });
-		return { puzzle };
+		switch (version) {
+			case '2':
+			default: {
+				const puzzle = generator.generate(params.seed, constraints);
+				return { puzzle };
+			}
+
+			case '1': {
+				const puzzle = generatePuzzle(params.seed, params.seed, constraints);
+				return { puzzle };
+			}
+		}
 	} catch (err: any) {
 		throw error(500, err?.message ?? 'unknown error');
 	}
