ec2bf989dc71f11f65799de4f68605346ea68c1a
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-10-17 01:38:31 +0300

    bump version (patch)

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		version: {
-			name: 'v1.10.3',
+			name: 'v1.10.4',
 			pollInterval: 5 * 60 * 1000 // every 5 minutes
 		},
 		serviceWorker: {
