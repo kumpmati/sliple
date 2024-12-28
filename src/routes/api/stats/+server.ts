2d5bf085c36de9fa357c55b7af7180750b2dd916
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-12-28 15:20:47 +0200

    adjust stats rate limiting

--- src/routes/api/stats/+server.ts
+++ src/routes/api/stats/+server.ts
@@ -17,8 +17,7 @@ export type PuzzleStats = {
 };
 
 const ratelimiter = new RateLimiter({
-	IP: [60, 'm'], // 1 per second per IP address
-	IPUA: [20, 'm'] // 20 per minute per IP address + user agent
+	IPUA: [2, 's'] // 20 per minute per IP address + user agent
 });
 
 const completionSchema = z.object({
