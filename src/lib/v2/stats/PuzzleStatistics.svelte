20b236e5aa87e8e06386b3afbc7e51029d07e0b1
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-12-28 15:03:18 +0200

    better loading styles to statistics

--- src/lib/v2/stats/PuzzleStatistics.svelte
+++ src/lib/v2/stats/PuzzleStatistics.svelte
@@ -4,6 +4,8 @@
 	import TablerStarFilled from '~icons/tabler/star-filled';
 	import DistributionChart from './DistributionChart.svelte';
 	import { calculatePercentile } from './percentile';
+	import TablerLoader2 from '~icons/tabler/loader-2';
+	import TablerAlertCircle from '~icons/tabler/alert-circle';
 
 	type Props = {
 		moves: number | null;
@@ -82,44 +84,66 @@
 		</div>
 	</div>
 
-	{#if globalsLoading}
-		<p class="text-center text-slate-400">Loading global stats...</p>
-	{:else if globalsError}
-		<p class="my-4 text-red-400">Error: {globalsError}</p>
-	{:else}
-		<p class="mt-8 text-sm font-normal text-slate-400">Global distribution</p>
-		<DistributionChart
-			data={globalDistribution}
-			numMoves={moves}
-			averageMoves={globalAverageMoves}
-			class="mt-4"
-		/>
+	<div class="relative grid w-full" style="grid-template-areas: 'a'">
+		{#if globalsLoading}
+			<div
+				class="absolute inset-0 left-0 right-0 z-10 scale-x-110 bg-slate-800/50 backdrop-blur-sm [grid-area:a]"
+			></div>
 
-		<table class="mt-16 w-full text-slate-400">
-			<tbody>
-				<tr>
-					<td class="py-1 font-normal text-green-400">Your percentile</td>
-					<td class="min-w-16 text-right font-bold text-green-400">
-						{#if percentile === null}
-							-
-						{:else if percentile < 1}
-							{'<1 %'}
-						{:else}
-							{Math.round(percentile)} %
-						{/if}
-					</td>
-				</tr>
-				<tr>
-					<td class="py-1 font-normal">
-						Average moves <span class="text-slate-600">(global)</span>
-					</td>
-					<td class="min-w-16 text-right font-bold text-white">{globalAverageMoves.toFixed(1)}</td>
-				</tr>
-				<tr>
-					<td class="py-1 font-normal">Solves <span class="text-slate-600">(global)</span></td>
-					<td class="min-w-16 text-right font-bold text-white">{globalCompletions}</td>
-				</tr>
-			</tbody>
-		</table>
-	{/if}
+			<p
+				class="z-10 inline-flex flex-col items-center justify-center gap-2 text-center text-slate-400 [grid-area:a]"
+			>
+				<TablerLoader2 class="size-8 animate-spin" />
+				Loading global statistics...
+			</p>
+		{:else if globalsError}
+			<div
+				class="absolute inset-0 left-0 right-0 z-10 scale-x-110 bg-slate-800/50 backdrop-blur-sm [grid-area:a]"
+			></div>
+			<p
+				class="z-10 inline-flex flex-col items-center justify-center gap-2 text-center text-red-400 [grid-area:a]"
+			>
+				<TablerAlertCircle class="size-8" />
+
+				{globalsError}
+			</p>
+		{/if}
+		<div class="flex w-full flex-col items-center [grid-area:a]">
+			<p class="mt-8 text-sm font-normal text-slate-400">Global distribution</p>
+			<DistributionChart
+				data={globalDistribution}
+				numMoves={moves}
+				averageMoves={globalAverageMoves}
+				class="mt-4"
+			/>
+
+			<table class="mt-16 w-full text-slate-400">
+				<tbody>
+					<tr>
+						<td class="py-1 font-normal text-green-400">Your percentile</td>
+						<td class="min-w-16 text-right font-bold text-green-400">
+							{#if percentile === null}
+								-
+							{:else if percentile < 1}
+								{'<1 %'}
+							{:else}
+								{Math.round(percentile)} %
+							{/if}
+						</td>
+					</tr>
+					<tr>
+						<td class="py-1 font-normal">
+							Average moves <span class="text-slate-600">(global)</span>
+						</td>
+						<td class="min-w-16 text-right font-bold text-white">{globalAverageMoves.toFixed(1)}</td
+						>
+					</tr>
+					<tr>
+						<td class="py-1 font-normal">Solves <span class="text-slate-600">(global)</span></td>
+						<td class="min-w-16 text-right font-bold text-white">{globalCompletions}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
