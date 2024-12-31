fee40f1b5aca7d49427cec0276ff09e242cdc27a
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-12-31 14:01:27 +0200

    fix: remove ssr from /random

--- src/routes/play/random/+page.ts
+++ src/routes/play/random/+page.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import { nanoid } from 'nanoid';
 
+export const ssr = false;
+
 export const load = () => {
 	redirect(302, `/play/random/${nanoid(16)}`);
 };
