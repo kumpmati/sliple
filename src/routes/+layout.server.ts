6b86f0910fe0b7aa4650a74a12ceffa1b63f55be
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-01-02 13:32:35 +0200

    test fetching timezone

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,13 +2,14 @@ import { error } from '@sveltejs/kit';
 import { analyzePuzzle } from '$lib/services/generator/analyze';
 import { generateDailyPuzzle } from '$lib/v2/generate';
 
-export const load = async () => {
+export const load = async (e) => {
 	try {
 		const puzzle = generateDailyPuzzle();
 
 		return {
 			puzzle,
-			analysis: analyzePuzzle(puzzle)
+			analysis: analyzePuzzle(puzzle),
+			timezone: e.request?.cf?.timezone
 		};
 	} catch (err: any) {
 		error(500, err?.message ?? 'unknown error');
