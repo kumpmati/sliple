1cf1f749480b9d050cd6b0a963f14a765d20b30b
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-01-02 14:10:07 +0200

    fix: possibly fix audio on ios

--- src/lib/services/sound/index.ts
+++ src/lib/services/sound/index.ts
@@ -8,13 +8,13 @@ export class GameAudio {
 
 	constructor() {
 		this.sounds = {
-			click: new Howl({ src: ['/sound/click.ogg', '/sound/click.mp3'], volume: 0.25 }),
-			swipe: new Howl({ src: ['/sound/swipe.ogg', '/sound/swipe.mp3'], volume: 0.25 }),
-			reset: new Howl({ src: ['/sound/reset.ogg', '/sound/reset.mp3'], volume: 0.5 }),
-			undo: new Howl({ src: ['/sound/undo.ogg', '/sound/undo.mp3'] }),
-			win: new Howl({ src: ['/sound/win.ogg', '/sound/win.mp3'] }),
-			lose: new Howl({ src: ['/sound/lose.ogg', '/sound/lose.mp3'] }),
-			hit: new Howl({ src: ['/sound/hit.ogg', '/sound/hit.mp3'] })
+			click: new Howl({ src: ['/sound/click.ogg', '/sound/click.mp3'], volume: 0.25, html5: true }),
+			swipe: new Howl({ src: ['/sound/swipe.ogg', '/sound/swipe.mp3'], volume: 0.25, html5: true }),
+			reset: new Howl({ src: ['/sound/reset.ogg', '/sound/reset.mp3'], volume: 0.5, html5: true }),
+			undo: new Howl({ src: ['/sound/undo.ogg', '/sound/undo.mp3'], html5: true }),
+			win: new Howl({ src: ['/sound/win.ogg', '/sound/win.mp3'], html5: true }),
+			lose: new Howl({ src: ['/sound/lose.ogg', '/sound/lose.mp3'], html5: true }),
+			hit: new Howl({ src: ['/sound/hit.ogg', '/sound/hit.mp3'], html5: true })
 		} as const;
 	}
 
